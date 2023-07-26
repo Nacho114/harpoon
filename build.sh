8cad71b92f562c307bcf55c725475762cee7d23b
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-07-26 18:08:47 +0200

    fixed build.sh typo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.config/zellij/plugins/
 cargo build --release
 mv target/wasm32-wasi/release/harpoon.wasm ~/.config/zellij/plugins/
 
-git clone git clone git@github.com:nacho114/cached.git
+git clone git@github.com:nacho114/cached.git
 cd cached
 cargo build --release
 mv target/wasm32-wasi/release/cached.wasm ~/.config/zellij/plugins/
